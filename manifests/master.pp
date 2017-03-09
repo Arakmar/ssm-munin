c4d4fc34154a8bc906152380123362770b37768e
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2017-03-09 10:29:37 +0100

    The host_name parameter of master doesn't need to be a domain name

--- manifests/master.pp
+++ manifests/master.pp
@@ -91,9 +91,6 @@ class munin::master (
 
   if $host_name {
     validate_string($host_name)
-    if ! is_domain_name($host_name) {
-      fail('host_name should be a valid domain name')
-    }
   }
 
   validate_array($extra_config)
